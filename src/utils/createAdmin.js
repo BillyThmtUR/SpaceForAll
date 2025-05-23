1aed5c1681bd0de4c9e95967da786a8006d855d5
BillyThmtUR <153058930+BillyThmtUR@users.noreply.github.com>
2025-05-23 16:06:29 +0400

    Update createAdmin.js

--- src/utils/createAdmin.js
+++ src/utils/createAdmin.js
@@ -1,10 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient(
-  'https://odkawbjuwndlbrjllrts.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ka2F3Ymp1d25kbGJyamxscnRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMTU1NjIsImV4cCI6MjA2MjY5MTU2Mn0.9kt_nK4P4nYNHvKYyfk9IOasDq-jpr_Z2ZbYCHPdoNM'
-);
-
 async function createAdmin() {
   const { data, error } = await supabase.auth.admin.createUser({
     email: 'billy.thomont@univ-reunion.fr',
